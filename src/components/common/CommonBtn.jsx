2d265a46315e9bf6e3cfb48bbc26e91e4281dc5d
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-11 13:49:07 +0530

    adding svg in button

--- src/components/common/CommonBtn.jsx
+++ src/components/common/CommonBtn.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { BtnArrow } from "./Icon";
 
 const CommonBtn = ({ className, btnName }) => {
   return (
     <div>
       <button
-        className={`${className} py-[12.5px] text-white px-5 bg-btngradient shadow-btnshadow rounded-full`}
+        className={`${className} py-[12.5px] text-white px-5 bg-btngradient shadow-btnshadow rounded-full flex justify-center items-center gap-[10px]`}
       >
-        {btnName}
+        {btnName} <BtnArrow />
       </button>
     </div>
   );
