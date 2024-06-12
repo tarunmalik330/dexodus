20e84cf1c37e3d244013a2e4d4008a08f1585717
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-12 06:22:55 +0530

    change padding bottom

--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -3,7 +3,7 @@ import CommonBtn from "./common/CommonBtn";
 
 const Ready = () => {
   return (
-    <div className="xl:pt-[140px] md:pt-[90px] sm:pt-[60px] pt-12">
+    <div className="xl:pt-[140px] md:pt-[90px] sm:pt-[60px] pt-12 xl:pb-[140p] md:pb-[90px] sm:pb-[60px] pb-12">
       <div className="container xl:max-w-[1164px] mx-auto xl:px-3 px-6">
         <div className="ready-border-box before:w-full before:h-full before:absolute before:-z-[1] before:p-[2px] before:pointer-events-none lg:before:rounded-full before:rounded-[60px] relative rounded-full">
           <div className="w-full bg-gradientlayer lg:rounded-full rounded-[60px] lg:h-[388px] min-[380px]:h-[340px] h-[220px] bg-cover bg-no-repeat bg-center flex justify-center items-center flex-col">
